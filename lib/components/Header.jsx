238bcc13554f5f83f36bf072730a5fb06b77ffc9
mgoodrich <mark.goodrich@live.com>
2018-05-04 15:18:35 -0400

    initial commit

---
+++ lib/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+            Hello World!
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
