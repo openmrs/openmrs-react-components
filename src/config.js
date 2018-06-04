e780e031a5d1a7909d509aae63178392444e5ea1
mgoodrich <mark.goodrich@live.com>
2018-06-04 15:20:35 -0400

    RAUI-11: Allow OpenMRS Endpoint URL to be configurable

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-// TODO ability to specify URL of server
+// this allows consuming create-react-app apps to inject the server name URL and context path (at build time) via the REACT_APP_CONTEXT_PATH variable,
+// see: https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#adding-custom-environment-variables
+// otherwise uses the current context path taken from examining the window object
 
-const contextPath = window.location.href.split('/')[3];
-const apiBaseUrl = `/${contextPath}/ws/rest/v1`;
-
-// TODO: pull this out into configuration for testing
-//const apiBaseUrl = "http://localhost:8080/openmrs/ws/rest/v1";
+const apiBaseUrl  = (typeof process !== 'undefined' && typeof process.env !== 'undefined' &&
+  typeof process.env.REACT_APP_CONTEXT_PATH  !== 'undefined' && process.env.REACT_APP_CONTEXT_PATH !== null) ?
+  `${process.env.REACT_APP_CONTEXT_PATH}/ws/rest/v1` : `/${window.location.href.split('/')[3]}/ws/rest/v1`;
 
 export const axiosConfig = {
   baseURL: apiBaseUrl,
