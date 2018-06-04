044ff6ae136ce402a91ec381503608dce33ffbf4
mgoodrich <mark.goodrich@live.com>
2018-06-04 15:40:44 -0400

    RAUI-11: Allow OpenMRS Endpoint URL to be configurable (refactor)

--- src/config.js
+++ src/config.js
@@ -1,12 +1,19 @@
 import axios from 'axios';
 
-// this allows consuming create-react-app apps to inject the server name URL and context path (at build time) via the REACT_APP_CONTEXT_PATH variable,
+// this allows consuming create-react-app apps to inject the server address URL and context path (at build time)
+// via the REACT_APP_SERVER_ADDRESS and REACT_APP_CONTEXT_PATH environment variables
 // see: https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#adding-custom-environment-variables
-// otherwise uses the current context path taken from examining the window object
+// otherwise doesn't set the address and gets the context path by examining the window object
 
-const apiBaseUrl  = (typeof process !== 'undefined' && typeof process.env !== 'undefined' &&
+const serverAddress = (typeof process !== 'undefined' && typeof process.env !== 'undefined' &&
+  typeof process.env.REACT_APP_SERVER_ADDRESS  !== 'undefined' && process.env.REACT_APP_SERVER_ADDRESS !== null) ?
+  process.env.REACT_APP_SERVER_ADDRESS  : "";
+
+const contextPath  = (typeof process !== 'undefined' && typeof process.env !== 'undefined' &&
   typeof process.env.REACT_APP_CONTEXT_PATH  !== 'undefined' && process.env.REACT_APP_CONTEXT_PATH !== null) ?
-  `${process.env.REACT_APP_CONTEXT_PATH}/ws/rest/v1` : `/${window.location.href.split('/')[3]}/ws/rest/v1`;
+  process.env.REACT_APP_CONTEXT_PATH : window.location.href.split('/')[3];
+
+const apiBaseUrl = `${serverAddress}${contextPath}/ws/rest/v1`;
 
 export const axiosConfig = {
   baseURL: apiBaseUrl,
