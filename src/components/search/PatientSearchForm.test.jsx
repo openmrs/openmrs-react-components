b190983fec4352e046d9c5fc45fdae61f084f6f6
Cosmin Ioan <cioan@pih.org>
2018-06-08 11:49:32 -0400

    RAUI-14, add component test

---
+++ src/components/search/PatientSearchForm.test.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import PatientSearchForm from './PatientSearchForm';
+
+
+
+describe ("patientSearchForm", () => {
+
+    const mockStore = configureMockStore();
+
+    it ("should render correctly", () => {
+
+        const store = mockStore({});
+
+        const rendered = renderer.create(
+            <Provider store={store}>
+                <PatientSearchForm />
+            </Provider>
+        );
+
+        expect(rendered.toJSON()).toMatchSnapshot();
+    });
+
+});
