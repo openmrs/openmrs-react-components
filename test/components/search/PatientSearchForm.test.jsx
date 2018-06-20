7ce8fb07c6fd9f9aeeee099d97714cedf9a0bf14
mgoodrich <mark.goodrich@live.com>
2018-06-20 10:40:15 -0400

    move tests into separate test directory

---
+++ test/components/search/PatientSearchForm.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import PatientSearchForm from '../../../src/components/search/PatientSearchForm';
+
+
+describe("patientSearchForm", () => {
+
+  const mockStore = configureMockStore();
+
+  it("should render correctly", () => {
+
+    const store = mockStore({});
+
+    const rendered = renderer.create(
+      <Provider store={store}>
+        <PatientSearchForm />
+      </Provider>
+    );
+
+    expect(rendered.toJSON()).toMatchSnapshot();
+  });
+
+});
