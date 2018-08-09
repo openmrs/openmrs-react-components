8e38fcf7f12cf8271369d2bf167425a25c089f9e
mgoodrich <mark.goodrich@live.com>
2018-08-09 16:19:05 -0400

    refactor Login to use Material-UI (work in progress)

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -3,43 +3,18 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import Button from '@material-ui/core/Button';
-import InputLabel from '@material-ui/core/InputLabel';
-import Typography from '@material-ui/core/Typography';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
-import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import {
+  Select,
+  TextField,
+} from 'redux-form-material-ui';
+
 import Errors from '../errors/Errors';
 
 
 let LoginForm = props => {
   const { handleSubmit, pristine, reset, submitting, locations } = props;
-
-  const renderSelect = ({
-    input,
-    meta: { touched, error },
-    children,
-    ...custom
-  }) => (
-    <Select
-      {...input}
-      onChange={(event, index, value) => input.onChange(value)}
-      children={children}
-      {...custom}
-    />
-  );
-
-  const renderTextField = ({
-    input,
-    meta: { touched, error },
-    ...custom
-  }) => (
-    <TextField
-      {...input}
-      {...custom}
-    />
-  );
-
 
   return (
     <div>
@@ -52,21 +27,17 @@ let LoginForm = props => {
 
         <form onSubmit={handleSubmit}>
           <div>
-            <Field name="username" component={renderTextField} label="Username" type="text" />
+            <Field name="username" component={TextField} label="Username" type="text" />
           </div>
           <div>
-            <Field name="password" component={renderTextField} label="Password" type="password"/>
+            <Field name="password" component={TextField} label="Password" type="password"/>
           </div>
           <div>
-            <FormControl>
-              <InputLabel>Location</InputLabel>
-              <Field id="location" name="location" options={ locations } component={renderSelect}>
-                <MenuItem value="" />
-                { locations.map(option =>
-                  <MenuItem key={option.uuid} value={option.uuid}>{option.display}</MenuItem>
-                )}
-              </Field>
-            </FormControl>
+            <Field name="location" component={Select}>
+              { locations.map(option =>
+                <MenuItem key={option.uuid} value={option.uuid}>{option.display}</MenuItem>
+              )}
+            </Field>
           </div>
           <div>
             <Button size="large" variant="contained" disabled={pristine || submitting} type="submit">Submit</Button>
