552882d901533654ee3e3e9a40d91deeb426c460
mgoodrich <mark.goodrich@live.com>
2018-08-10 10:56:55 -0400

    Revert: rework Login to use material-ui

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -2,55 +2,105 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
-import Button from '@material-ui/core/Button';
-import MenuItem from '@material-ui/core/MenuItem';
-import Typography from '@material-ui/core/Typography';
-import {
-  Select,
-  TextField,
-} from 'redux-form-material-ui';
-
+import FieldInput from '../form/FieldInput';
 import Errors from '../errors/Errors';
+import { Button, ButtonToolbar, Grid, Row, Col, Form, FormGroup, FormControl, ControlLabel, Label } from 'react-bootstrap';
 
 
 let LoginForm = props => {
   const { handleSubmit, pristine, reset, submitting, locations } = props;
 
+  const Select = ({ input, options, disabled }) => (
+    <div>
+      <select {...input} disabled={disabled}>
+        { options.map(option =>
+          <option key={option.uuid} value={option.uuid}>
+            {option.display}
+          </option>
+        )}
+      </select>
+
+    </div>
+  );
+
   return (
     <div>
-      <div>
-        <Typography variant="headline">Login</Typography>
-      </div>
-      <Errors />
+      <h3><Label>Login</Label></h3>
+      <Errors/>
+      <Form horizontal onSubmit={handleSubmit}>
+        <Grid>
 
-      <div>
+          <Row>
+            <FormGroup controlId="formUsername">
+              <Col componentClass={ControlLabel} sm={2}>
+                Username
+              </Col>
+              <Col sm={4}>
+                <Field name="username" type='text' component={FieldInput} placeholder="username"  />
+              </Col>
+            </FormGroup>
+          </Row>
 
-        <form onSubmit={handleSubmit}>
-          <div>
-            <Field name="username" component={TextField} label="Username" type="text" />
-          </div>
-          <div>
-            <Field name="password" component={TextField} label="Password" type="password"/>
-          </div>
-          <div>
-            <Field name="location" component={Select}>
-              { locations.map(option =>
-                <MenuItem key={option.uuid} value={option.uuid}>{option.display}</MenuItem>
-              )}
-            </Field>
-          </div>
-          <div>
-            <Button size="large" variant="contained" disabled={pristine || submitting} type="submit">Submit</Button>
-            <Button size="large" variant="contained" disabled={pristine || submitting} onClick={reset}>Clear Values</Button>
-          </div>
-        </form>
-      </div>
+          <Row>
+            <FormGroup controlId="formPassword">
+              <Col componentClass={ControlLabel} sm={2}>
+                Password
+              </Col>
+              <Col sm={4}>
+                <Field name="password" type='password' component={FieldInput} placeholder="password"  />
+              </Col>
+            </FormGroup>
+          </Row>
+
+          <Row>
+            <FormGroup controlId="formLocationSelect">
+              <Col componentClass={ControlLabel} sm={2}>
+                Location
+              </Col>
+              <Col sm={4}>
+                <Field name="location" options={ locations } component={Select} >
+
+                </Field>
+
+              </Col>
+            </FormGroup>
+          </Row>
+
+          <Row>
+            <FormGroup controlId="formSubmit">
+            <Col smOffset={2} sm={4}>
+            <ButtonToolbar>
+                <Button
+                  bsStyle="success"
+                  bsSize="large"
+                  disabled={pristine || submitting}
+                  type="submit"
+                >
+                  Submit
+                </Button>
+
+
+                <Button
+                  bsStyle="danger"
+                  bsSize="large"
+                  disabled={pristine || submitting}
+                  onClick={reset}
+                >
+                  Clear Values
+                </Button>
+            </ButtonToolbar>
+            </Col>
+            </FormGroup>
+          </Row>
+        </Grid>
+      </Form>
     </div>
   );
 };
 
 LoginForm.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
+  pristine: PropTypes.bool.isRequired,
   reset: PropTypes.func.isRequired,
   submitting: PropTypes.bool.isRequired,
   locations: PropTypes.array.isRequired
