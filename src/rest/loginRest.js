415592e3b51e8a540f50068c026f19a7f8c0d536
shao yuan <shaoyuan_chewchia@college.harvard.edu>
2018-08-14 09:28:17 -0400

    Revert authentication back to username and password being sent in every header

--- src/rest/loginRest.js
+++ src/rest/loginRest.js
@@ -3,8 +3,14 @@ import { axiosInstance } from "../config";
 const api = {
 
   login: (params) => {
-    var authStr = "Basic " + btoa(params.username + ':' + params.password);
-    return axiosInstance.get('session', { headers: { Authorization: authStr}})
+    // Authentication via cookie and not by setting default headers attempt
+    // var authStr = "Basic " + btoa(params.username + ':' + params.password);
+    // return axiosInstance.get('session', { headers: { Authorization: authStr } }).then(function (response) {
+    //   return response.data;
+    // });
+
+    axiosInstance.defaults.headers.common['Authorization'] = "Basic " + btoa(params.username + ':' + params.password);
+    return axiosInstance.get('session')
       .then((response) => response.data);
   },
 
