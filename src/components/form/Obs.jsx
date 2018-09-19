85b3d766ef018df2f03931a2cdab300c17fbeec4
mgoodrich <mark.goodrich@live.com>
2018-09-19 16:55:03 -0400

    add PropTypes to obs; set type based on datatype directly

--- src/components/form/Obs.jsx
+++ src/components/form/Obs.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Field } from 'redux-form';
+import PropTypes from 'prop-types';
 import ButtonGroup from './ButtonGroup';
 import FieldInput from "./FieldInput";
 
 const Obs = (props) => {
+
+  // TODO: type should be controlled based on datatype of concept
 
   if ( typeof props.conceptAnswers !== 'undefined' ) {
     return (
@@ -12,30 +15,39 @@ const Obs = (props) => {
         component={ ButtonGroup }
         options={ props.conceptAnswers } />
     );
-  } else if ( typeof props.datatype !== 'undefined' && props.datatype === 'text') {
-    return (
-      // TODO: type should be controlled based on datatype of concept
-      <Field
-        name={`obs|path=${props.path}|concept=${props.concept}`}
-        type='text'
-        component={ FieldInput }
-        placeholder={ props.placeholder }
-        validate={ props.validate }
-        warn={ props.warn }/>
-    )
   } else {
     return (
-      // TODO: type should be controlled based on datatype of concept
       <Field
         name={`obs|path=${props.path}|concept=${props.concept}`}
-        type='number'
-        component={ FieldInput }
-        placeholder={ props.placeholder }
-        validate={ props.validate }
-        warn={ props.warn } />
-    )
+        type={props.datatype}
+        component={FieldInput}
+        placeholder={props.placeholder}
+        validate={props.validate}
+        value={props.value}
+        warn={props.warn} />
+    );
   }
+};
 
+Obs.propTypes = {
+  concept: PropTypes.string.isRequired,
+  conceptAnswers: PropTypes.array,
+  datatype: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  validate: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.func]),
+  value:  PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number]),
+  warn: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.func]),
+};
+
+Obs.defaultProps = {
+  datatype: 'number'
 };
 
 export default Obs;
