1989663772f50ebd57d2102454c887958bc062eb
mgoodrich <mark.goodrich@live.com>
2018-09-25 14:39:00 -0400

    add FormContext to Submit button

--- src/components/form/Submit.jsx
+++ src/components/form/Submit.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import FormContext from './FormContext';
 
 const Submit = (props) => {
 
-  const { submitting } = props;
-
   return (
-    <Button
-      bsSize="large"
-      bsStyle="success"
-      disabled={submitting}
-      type="submit"
-    >
-      Submit
-    </Button>
+    <FormContext.Consumer>
+      {context =>
+        <Button
+          bsSize="large"
+          bsStyle="success"
+          disabled={context.submitting}
+          type="submit"
+        >
+          Submit
+        </Button>
+      }
+    </FormContext.Consumer>
   );
 };
 
