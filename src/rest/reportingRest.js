a193386c9f9b9f7ca96a5d8a2fe86cb21f4863cf
Cosmin Ioan <cioan@pih.org>
2018-10-03 11:11:40 -0400

    MLW-774, add rest call to retrieve appointment data

--- src/rest/reportingRest.js
+++ src/rest/reportingRest.js
@@ -16,6 +16,22 @@ const api = {
           return response.data;
         }
       });
+  },
+
+  getIC3Appt: (params) => {
+    return axiosInstance.get("pihmalawi/ic3"
+      + ( params.endDate ? "?endDate=" + params.endDate : '')
+      + ( params.location ? "&location=" + params.location : '')
+      + ( params.patient ? "&patient=" + params.patient : '')
+    )
+      .then((response) => {
+        if (response.status !== 200) {
+          throw response;
+        }
+        else {
+          return response.data;
+        }
+      });
   }
 
 };
