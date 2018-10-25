9fc6d1b4c26569926cb4064e7520318ab277d543
rowland henshaw <rowland.henshaw@gmail.com>
2018-10-25 13:55:48 +0100

    added default representation to order rest

--- src/domain/encounter/constants.js
+++ src/domain/encounter/constants.js
@@ -3,3 +3,4 @@
 // TODO this will have to be to modified based on GP when to use form field vs comment
 // TODO is there a better way to handle rep of nested group members?
 export const DEFAULT_ENCOUNTER_REP = '(id,uuid,encounterDatetime,location:(id,uuid,name),encounterType:(id,uuid,name),obs:(id,uuid,value,concept,comment,display,groupMembers:(id,uuid,value,concept,comment,display,groupMembers:(id,uuid,value,concept,comment,display,groupMembers)))';
+export const DEFAULT_ORDER_REP = '(id,uuid,concept,orderNumber,dateActivated,urgency,display,patient,orderType)';
--- src/rest/orderRest.js
+++ src/rest/orderRest.js
@@ -1,7 +1,8 @@
 import { axiosInstance } from '../config';
+import { DEFAULT_ORDER_REP } from "../domain/encounter/constants";
 
 const api = {
-  fetchActiveOrdersByPatient: patientUUID => axiosInstance.get(`order?totalCount=true&sort=desc&status=active&patient=${patientUUID}&v=full`)
+  fetchActiveOrdersByPatient: patientUUID => axiosInstance.get(`order?totalCount=true&sort=desc&status=active&patient=${patientUUID}&v=custom:${DEFAULT_ORDER_REP}`)
     .then((response) => {
       if (response.status !== 200) {
         throw response;
