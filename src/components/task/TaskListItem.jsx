3c93c49dc078b71d513a534acdbc3d91db3d14f0
mgoodrich <mark.goodrich@live.com>
2018-10-31 17:05:32 -0400

    update TaskListItem styling

--- src/components/task/TaskListItem.jsx
+++ src/components/task/TaskListItem.jsx
@@ -7,30 +7,43 @@ let TaskListItem = props => {
 
   const completed = props.completed ? props.completed(props.patient) : false;
 
+  let taskListItemStyle = {
+    'border': 0,
+    'color': completed ? props.colorComplete : props.colorPending
+  };
+
   return (
-    <ListGroupItem href={props.link ? '#' + props.link : ''}>
-      { completed &&
-      <FontAwesomeIcon icon="check" />
-      }
-      { !completed &&
-      <FontAwesomeIcon icon="arrow-right" />
-      }
+    <ListGroupItem
+      href={props.link ? '#' + props.link : ''}
+      style={taskListItemStyle}
+    >
+      <FontAwesomeIcon
+        icon="check"
+        style={{ visibility: completed ? "visible" : "hidden" }}
+      />
       &nbsp;{props.title}
+      { props.timeFn && (
+        bull
+        )
+      }
     </ListGroupItem>
   );
 
 };
 
 TaskListItem.propTypes = {
+  colorComplete: PropTypes.string,
+  colorPending: PropTypes.string,
   completed: PropTypes.func.isRequired,
   link: PropTypes.string,
   patient: PropTypes.object.isRequired,
   title: PropTypes.string.isRequired,
+  timeFn: PropTypes.fn
 };
 
 TaskListItem.defaultProps = {
-  completedFilters: [() => false],
-  requiredFilters: [() => false]
+  colorComplete: "green",
+  colorPending: "gray1"
 };
 
 export default TaskListItem;
