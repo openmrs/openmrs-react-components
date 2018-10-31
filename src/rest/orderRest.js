dead8471f973a1d16df96053614cf3b424abf877
rowland henshaw <rowland.henshaw@gmail.com>
2018-10-31 12:35:26 +0100

    added ordertype to orderRest call

--- src/rest/orderRest.js
+++ src/rest/orderRest.js
@@ -2,7 +2,7 @@ import { axiosInstance } from '../config';
 import { DEFAULT_ORDER_REP } from "../domain/encounter/constants";
 
 const api = {
-  fetchActiveOrdersByPatient: patientUUID => axiosInstance.get(`order?totalCount=true&sort=desc&status=active&patient=${patientUUID}&v=custom:${DEFAULT_ORDER_REP}`)
+  fetchActiveOrdersByPatient: (patientUUID, orderType) => axiosInstance.get(`order?totalCount=true&sort=desc&status=active&patient=${patientUUID}&orderType=${orderType}&v=custom:${DEFAULT_ORDER_REP}`)
     .then((response) => {
       if (response.status !== 200) {
         throw response;
