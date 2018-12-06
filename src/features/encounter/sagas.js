be4bb961b1e532b686c4ec9bb34d6761608b802c
mgoodrich <mark.goodrich@live.com>
2018-12-06 16:49:12 +0300

    rename import

--- src/features/encounter/sagas.js
+++ src/features/encounter/sagas.js
@@ -1,13 +1,13 @@
 import { call, put, takeLatest } from "redux-saga/effects";
 import FETCH_ENCOUNTERS_TYPES from "./types";
 import fetchEncountersActions from './actions';
-import encounterApi from "../../rest/encounterRest";
+import encounterRest from "../../rest/encounterRest";
 
 
 function* fetchEncounters(action) {
   try {
 
-    let response = yield call(encounterApi.fetchEncountersByPatient, {
+    let response = yield call(encounterRest.fetchEncountersByPatient, {
       patient: action.patient,
       encounterType: action.encounterType
     });
