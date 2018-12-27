52871455cc4cb7f2c1d3ddc83864d3666c47672e
mgoodrich <mark.goodrich@live.com>
2018-12-27 07:35:11 -0500

    fix react-redux connect import

--- src/components/obs/ObsValue.jsx
+++ src/components/obs/ObsValue.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import connect from "react-redux/es/connect/connect";
+import { connect } from "react-redux";
 import validations from "../../features/form/validations";
 import { selectors } from "../../store";
 import { conceptActions } from "../../features/concept";
