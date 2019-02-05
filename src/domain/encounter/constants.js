b93533e57ae39b194b3d2e1e11ca5aba2aed7d23
mgoodrich <mark.goodrich@live.com>
2019-02-05 15:05:40 -0500

    add in custom ref for "value" of obs in default_encounter_rep

--- src/domain/encounter/constants.js
+++ src/domain/encounter/constants.js
@@ -4,7 +4,7 @@
 // TODO is there a better way to handle rep of nested group members?
 
 export const DEFAULT_ENCOUNTER_REP = '(id,uuid,encounterDatetime,location:(id,uuid,name),encounterType:(id,uuid,name),' +
-  'obs:(id,uuid,value,concept:(uuid,display,name,datatype,units),comment,display,' +
-  'groupMembers:(id,uuid,value,concept:(uuid,display,name,datatype,units),comment,display,' +
-  'groupMembers:(id,uuid,value,concept:(uuid,display,name,datatype,units),comment,display,groupMembers)))';
+  'obs:(id,uuid,value:(id,uuid,display,name:(uuid,name)),concept:(uuid,display,name,datatype,units),comment,display,' +
+  'groupMembers:(id,uuid,value:(id,uuid,display,name:(uuid,name)),concept:(uuid,display,name,datatype,units),comment,display,' +
+  'groupMembers:(id,uuid,value:(id,uuid,display,name:(uuid,name)),concept:(uuid,display,name,datatype,units),comment,display,groupMembers)))';
 export const DEFAULT_ORDER_REP = '(id,uuid,concept,orderNumber,dateActivated,urgency,display,patient,orderType)';
