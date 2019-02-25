0ea886a4ac0adac6362ee397e950307a3e049cf3
rowland henshaw <rowland.henshaw@gmail.com>
2019-02-25 19:45:01 +0100

    added id attribute to button group OBS

--- src/components/widgets/ButtonGroup.jsx
+++ src/components/widgets/ButtonGroup.jsx
@@ -3,6 +3,10 @@ import { ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
 import '../../../assets/css/widgets.css';
 import formUtil from '../../features/form/util';
 
+const formatId = (display) => {
+  return display.toLowerCase().replace(/\s/g,'_');
+};
+
 const buttonStyle = {
   whiteSpace: "normal"
 };
@@ -16,14 +20,19 @@ const ButtonGroup = ({
 
   const edit = (
     <ToggleButtonGroup type="radio" justified={true} {...input}>
-      {options.map(option =>
-        <ToggleButton
-          key={option.uuid}
-          style={buttonStyle}
-          value={option.uuid}
-        >
-          {option.name || option.display}
-        </ToggleButton>
+      {options.map((option) => {
+        const displayId = formatId(option.display);
+        return (
+          <ToggleButton
+            id={displayId}
+            key={option.uuid}
+            style={buttonStyle}
+            value={option.uuid}
+          >
+            {option.name || option.display}
+          </ToggleButton>
+        );
+      }
       )}
     </ToggleButtonGroup>
   );
