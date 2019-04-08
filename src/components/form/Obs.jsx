d9ce7f790bc9b94d13db297ceec7b6cd7a2c9367
rowland henshaw <rowland.henshaw@gmail.com>
2019-04-08 17:39:47 +0100

    UHM-3829: added viewing validation messages to Obs componenents

--- src/components/form/Obs.jsx
+++ src/components/form/Obs.jsx
@@ -47,9 +47,13 @@ class Obs extends React.PureComponent {
 
   render() {
     const { required } = this.props;
+    
+    const defaultValidations = this.props.validate || this.state.getValidationAbsoluteRange;
+    const defaultWarnings = this.props.warn || this.state.getValidationAbnormalRange;
+    const validations = required ? defaultValidations.concat(formValidations.isRequired) : defaultValidations;
+    const warnings = required ? defaultWarnings.concat(formValidations.isRequired) : defaultWarnings;
+
     if (this.props.datatype === 'date') {
-      const defaultValidations = this.props.validate || this.state.getValidationAbsoluteRange;
-      const validations = required ? defaultValidations.concat(formValidations.isRequired) : defaultValidations;
       return (
         <Field
           component={CustomDatePicker}
@@ -86,8 +90,6 @@ class Obs extends React.PureComponent {
       );
     } else if (typeof this.props.conceptAnswers !== 'undefined') {
       if (this.props.widget === 'dropdown') {
-        const defaultValidations = this.props.validate || [];
-        const validations = required ? defaultValidations.concat(formValidations.isRequired) : defaultValidations;
         return (
           <Field
             component={Dropdown}
@@ -115,10 +117,6 @@ class Obs extends React.PureComponent {
         );
       }
     } else {
-      const defaultValidations = this.props.validate || this.state.getValidationAbsoluteRange;
-      const defaultWarnings = this.props.warn || this.state.getValidationAbnormalRange;
-      const validations = required ? defaultValidations.concat(formValidations.isRequired) : defaultValidations;
-      const warnings = required ? defaultWarnings.concat(formValidations.isRequired) : defaultWarnings;
       
       return (
         <div>
