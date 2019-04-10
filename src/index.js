e0a881f84f6a893eb12bda9dedc32c322e81a3cd
Abdo <abdopoetry@gmail.com>
2019-04-10 16:04:44 +0200

    Order index imports in bulks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import { all } from 'redux-saga/effects';
-
 import Accordion from './components/accordion/Accordion';
 import AuthenticatedRoute from './components/routes/AuthenticatedRoute';
 import BasicLayout from './components/layout/BasicLayout';
@@ -17,6 +15,7 @@ import EncounterHistory from './components/encounter/EncounterHistory';
 import encountersByEncounterTypeFilter from './domain/encounter/filters/encountersByEncounterTypeFilter';
 import Errors from './components/errors/Errors';
 import FieldInput from './components/widgets/FieldInput';
+import FormContext from './components/form/FormContext';
 import Head from './components/header/Head';
 import Header from './components/header/Header';
 import HeaderAlt from './components/header/HeaderAlt';
@@ -29,15 +28,13 @@ import LocationMenu from './components/header/LocationMenu';
 import Login from './components/login/Login';
 import LoginPage from './components/login/LoginPage';
 import Logout from './components/login/Logout';
+import { mountWithIntl, shallowWithIntl } from './components/localization/test/helpers/intl-test';
 import Obs from './components/form/Obs';
 import ObsGroup from './components/form/ObsGroup';
 import ObsHistory from './components/obs/ObsHistory';
 import ObsValue from './components/obs/ObsValue';
 import PatientCard from './components/patient/PatientCard';
 import PatientHeader from './components/header/PatientHeader';
-import patientObjByEncounterTypeAndObsFilter from './domain/patient/filters/patientObjByEncounterTypeAndObsFilter';
-import patientObjByEncounterTypeFilter from './domain/patient/filters/patientObjByEncounterTypeFilter';
-import patientObjByVisitLocationFilter from './domain/patient/filters/patientObjByVisitLocationFilter';
 import PatientSearch from './components/search/PatientSearch';
 import patientUtil from './domain/patient/patientUtil';
 import ProgramEnrollment from './components/program/ProgramEnrollment';
@@ -47,58 +44,27 @@ import Submit from './components/form/Submit';
 import SystemAlert from './components/system/SystemAlert';
 import TaskList from './components/task/TaskList';
 import ToolTip from './components/tooltip/ToolTip';
-import visitRestRepToPatientObjConverter from './domain/patient/converters/visitRestRepToPatientObjConverter';
+import withLocalisation, { setLocaleMessages } from './components/localization/withLocalisation';
 
-import {
-  SESSION_TYPES,
-  sessionSagas,
-  sessionActions
-} from './features/session/';
-import { systemWatcherSaga } from './features/system';
-import { LOGIN_TYPES, loginSagas, loginActions } from './features/login';
-import {
-  openmrsFormSagas,
-  formActions,
-  formValidations,
-  formUtil,
-  FORM_STATES
-} from './features/form';
-import { headerSagas, headerActions } from './features/header';
-import { errorsActions } from './features/errors';
-import { VISIT_TYPES, visitActions, visitSagas } from './features/visit';
-import { GRID_TYPES, gridActions } from './features/grid';
-import { patientActions, PATIENT_TYPES } from './features/patient';
-import { locationActions, locationSagas } from './features/location';
+import patientObjByEncounterTypeAndObsFilter from './domain/patient/filters/patientObjByEncounterTypeAndObsFilter';
+import patientObjByEncounterTypeFilter from './domain/patient/filters/patientObjByEncounterTypeFilter';
+import patientObjByVisitLocationFilter from './domain/patient/filters/patientObjByVisitLocationFilter';
+
 import { conceptSagas, conceptActions } from './features/concept';
-import {
-  globalPropertySagas,
-  globalPropertyActions
-} from './features/globalproperty';
-import {
-  patientIdentifierTypesActions,
-  patientIdentifierTypesSagas
-} from './features/patientIdentifierTypes';
-import { systemActions } from './features/system';
-import {
-  PATIENT_SEARCH_TYPES,
-  patientSearchActions,
-  patientSearchSagas
-} from './features/search/';
-import FormContext from './components/form/FormContext';
+import { errorsActions } from './features/errors';
+import { formActions, formValidations, formUtil, FORM_STATES, openmrsFormSagas } from './features/form';
+import { GRID_TYPES, gridActions } from './features/grid';
+import { globalPropertySagas, globalPropertyActions } from './features/globalproperty';
+import { headerSagas, headerActions } from './features/header';
+import { locationActions, locationSagas } from './features/location';
+import { LOGIN_TYPES, loginSagas, loginActions } from './features/login';
+import { patientActions, PATIENT_TYPES } from './features/patient';
+import { patientIdentifierTypesActions, patientIdentifierTypesSagas } from './features/patientIdentifierTypes';
+import { PATIENT_SEARCH_TYPES, patientSearchActions, patientSearchSagas } from './features/search/';
+import { SESSION_TYPES, sessionSagas, sessionActions } from './features/session/';
+import { systemActions,systemWatcherSaga } from './features/system';
+import { VISIT_TYPES, visitActions, visitSagas } from './features/visit';
 
-import { axiosInstance } from './config';
-import encounterRest from './rest/encounterRest';
-import orderRest from './rest/orderRest';
-import obsRest from './rest/obsRest';
-import patientRest from './rest/patientRest';
-import loginRest from './rest/loginRest';
-import sessionRest from './rest/sessionRest';
-import locationRest from './rest/locationRest';
-import visitRest from './rest/visitRest';
-import reportingRest from './rest/reportingRest';
-import conceptRest from './rest/conceptRest';
-import { reducers, selectors } from './store';
-import { library as fontAwesomeLibrary } from '@fortawesome/fontawesome-svg-core';
 import {
   faCaretDown,
   faTimes,
@@ -108,14 +74,27 @@ import {
   faArrowRight,
   faPencilAlt
 } from '@fortawesome/free-solid-svg-icons';
+import { library as fontAwesomeLibrary } from '@fortawesome/fontawesome-svg-core';
 
-import withLocalisation, {
-  setLocaleMessages
-} from './components/localization/withLocalisation';
-import {
-  mountWithIntl,
-  shallowWithIntl
-} from './components/localization/test/helpers/intl-test';
+import conceptRest from './rest/conceptRest';
+import encounterRest from './rest/encounterRest';
+import locationRest from './rest/locationRest';
+import loginRest from './rest/loginRest';
+import orderRest from './rest/orderRest';
+import obsRest from './rest/obsRest';
+import patientRest from './rest/patientRest';
+import sessionRest from './rest/sessionRest';
+import visitRest from './rest/visitRest';
+import reportingRest from './rest/reportingRest';
+
+import visitRestRepToPatientObjConverter from './domain/patient/converters/visitRestRepToPatientObjConverter';
+
+import { all } from 'redux-saga/effects';
+
+import { axiosInstance } from './config';
+
+import { reducers, selectors } from './store';
+
 
 fontAwesomeLibrary.add(
   faCaretDown,
