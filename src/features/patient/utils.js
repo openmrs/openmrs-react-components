5e78bf650a0a009f8f5c6954fceaf0423542ba96
jomadoye <omadoyejed@gmail.com>
2019-05-08 19:37:08 +0100

    IS-376: Update patient age format

--- src/features/patient/utils.js
+++ src/features/patient/utils.js
@@ -1,29 +1,32 @@
 import dateFns from 'date-fns';
 
 export const formatAge = (birthday) => {
-  const totalAgeInMonths = dateFns.differenceInCalendarMonths(new Date(), new Date(birthday));
+  const totalAgeInMonths = dateFns.differenceInMonths(new Date(), new Date(birthday));
+  let age = '';
   let ageInYears = parseInt( totalAgeInMonths / 12 );
   let ageInMonths = parseInt( totalAgeInMonths % 12 );
 
-  if (ageInYears < 2 && ageInMonths < 2){
-    ageInMonths = `${ageInMonths} month,`;
-  } else if (ageInYears > 1 || ageInMonths === 0){
-    ageInMonths = ''; 
+  if (ageInYears < 2){
+    ageInMonths = `${ageInMonths} month(s)`;
   } else {
-    ageInMonths = `${ageInMonths} months,`; 
+    ageInMonths = ''; 
   }
 
   if (ageInYears === 0) {
     ageInYears = '';
   } else if (ageInYears < 2){
     ageInYears = `${ageInYears} year,`;
-  } else {
+  } else if (ageInMonths){
+    // Check if there is month so trailing comma can be added
     ageInYears = `${ageInYears} years,`; 
+  } else {
+    ageInYears = `${ageInYears} years`; 
   }
+
+  age = `${ageInYears} ${ageInMonths}`;
   
   return {
-    ageInYears,
-    ageInMonths,
+    age,
     totalAgeInMonths
   };
 };
