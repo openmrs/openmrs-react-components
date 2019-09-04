3a94abd1de6155fe512850c695596c9083652c6c
mgoodrich <mark.goodrich@live.com>
2019-09-04 18:21:23 -0400

    RAUI-40: Order REST: add methods to save a new order and update order fulfiller status

--- src/rest/orderRest.js
+++ src/rest/orderRest.js
@@ -1,5 +1,5 @@
 import { axiosInstance } from '../config';
-import { DEFAULT_ORDER_REP } from "../domain/encounter/constants";
+import { DEFAULT_ORDER_REP} from "../domain/encounter/constants";
 
 const api = {
   fetchAllOrdersByPatient: (patientUUID, orderType) => axiosInstance.get(`order?s=default&totalCount=true&sort=desc&patient=${patientUUID}&orderTypes=${orderType}&v=custom:${DEFAULT_ORDER_REP}`)
@@ -10,6 +10,30 @@ const api = {
         return response.data;
       }
     }),
+
+  createOrder: (order, representation) => {
+    return axiosInstance.post(`order/?v=custom:${representation || DEFAULT_ORDER_REP}`, order)
+      .then((response) => {
+        if (response.status != 201) {
+          throw response;
+        } else {
+          return response.data;
+        }
+      });
+  },
+
+  // note that this endpoint is only availabe in Core 2.2 and above
+  updateFulfillerDetails: (order, details) => {
+    return axiosInstance.post(`order/${order.uuid}/fulfillerdetails`, details)
+      .then((response) => {
+        if (response.status != 201) {
+          throw response;
+        } else {
+          return response.data;
+        }
+      });
+  },
+
 };
 
 export default api;
