19127fd4438062881ff004de5f89631db98f40d0
Cosmin Ioan <cioan@pih.org>
2019-09-27 14:57:51 -0400

    UHM-4062, update absolute range validations

--- src/features/form/validations.jsx
+++ src/features/form/validations.jsx
@@ -66,9 +66,9 @@ const generateAbsoluteRangeValidators = concept => {
   } = concept;
   let hiAbsoluteRange, lowAbsoluteRange;
 
-  if (hiAbsolute || lowAbsolute) {
-    hiAbsoluteRange = hiAbsolute ? maxValue(hiAbsolute) : undefined;
-    lowAbsoluteRange = hiAbsolute ? minValue(lowAbsolute) : undefined;
+  if ((typeof hiAbsolute !== 'undefined' && hiAbsolute !== null)  || (typeof lowAbsolute !== 'undefined' && lowAbsolute !== null)) {
+    hiAbsoluteRange = (typeof hiAbsolute !== 'undefined' && hiAbsolute !== null) ? maxValue(hiAbsolute) : undefined;
+    lowAbsoluteRange = (typeof lowAbsolute !== 'undefined' && lowAbsolute !== null) ? minValue(lowAbsolute) : undefined;
     return [hiAbsoluteRange, lowAbsoluteRange].filter(Boolean);
   } else {
     return [];
