e3ff784617b79af100ec6686ec5298ff108673cc
Cosmin Ioan <cioan@pih.org>
2019-10-02 09:35:46 -0400

    UHM-4040, update validations test case

--- src/features/form/__tests__/validations.test.jsx
+++ src/features/form/__tests__/validations.test.jsx
@@ -1,25 +1,34 @@
+import React from 'react';
+import LocalizedMessage from "../../../components/localization/LocalizedMessage";
 import formValidations from "../validations.jsx";
+
+const localizedErrorMessage = <LocalizedMessage defaultMessage="Decimals not allowed" id="reactcomponents.value.disallow.decimals"/>;
 
 describe('Validation functions', () => {
 
   it('disallowDecimals when input value has decimals', () => {
 
-    expect(formValidations.disallowDecimals()("10.6")).toEqual('Decimals not allowed');
+    expect(formValidations.disallowDecimals()("10.6")).toEqual(localizedErrorMessage);
   });
 
   it('disallowDecimals when input value has spaces in the middle', () => {
 
-    expect(formValidations.disallowDecimals()("9  6")).toEqual('Decimals not allowed');
+    expect(formValidations.disallowDecimals()("9  6")).toEqual(localizedErrorMessage);
   });
 
   it('disallowDecimals when input value ends with comma', () => {
 
-    expect(formValidations.disallowDecimals()("956.")).toEqual('Decimals not allowed');
+    expect(formValidations.disallowDecimals()("956.")).toEqual(localizedErrorMessage);
   });
 
   it('disallowDecimals when input value contains non digit characters', () => {
 
-    expect(formValidations.disallowDecimals()("9_xxR_6")).toEqual('Decimals not allowed');
+    expect(formValidations.disallowDecimals()("9_xxR_6")).toEqual(localizedErrorMessage);
+  });
+
+  it('allowDecimals when input value is 0', () => {
+
+    expect(formValidations.disallowDecimals()("0")).toEqual(localizedErrorMessage);
   });
 
   it('allowDecimals when input value is an integer', () => {
