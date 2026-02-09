b04e859b9050a19340c60bae8d43fbdb40354533
Michael Seaton <mseaton@pih.org>
2026-02-09 14:58:08 -0500

    Update comments

--- src/constants.js
+++ src/constants.js
@@ -8,6 +8,9 @@ export const LOCATION_TYPES = {
   }
 };
 
+// Any date observation that needs to be handled like a datetime by REST when submitting should be added here
+// Ideally we wouldn't have this, and would just go based on the concept metadata, but that would require a greater
+// amount of refactoring than we want to do in this deprecated codebase.  If this becomse active again, we can revisit
 export const DATETIME_CONCEPTS = [
   "6234d61b-4c77-4af6-9bbb-533e44c03f24" // Date specimen received at laboratory
 ]
