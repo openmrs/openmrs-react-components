f0ef4f7e5e688d15b22a846c238df15b3a3a6b1b
Michael Seaton <mseaton@pih.org>
2026-07-30 12:50:00 -0400

    UHM-9418 - Fetch formFieldNamespace/formFieldPath in default obs/encounter reps

--- src/domain/obs/constants.js
+++ src/domain/obs/constants.js
@@ -3,7 +3,7 @@
 // TODO how do we handle fetching nested obs?
 // TODO now that we've extracted concept out into store, do we really need to fetch display?
 // TODO do we really need the ids here?
-export const DEFAULT_OBS_REP = '(id,uuid,display,obsDatetime,comment,value:(id,uuid,display,name:(uuid,name)),' +
+export const DEFAULT_OBS_REP = '(id,uuid,display,obsDatetime,comment,formFieldNamespace,formFieldPath,value:(id,uuid,display,name:(uuid,name)),' +
   'concept:(uuid),encounter:(id,uuid,encounterDatetime,encounterType:(uuid)),groupMembers:(uuid),obsGroup:(uuid))';
 
 export const DELETED_OBS_REP='(id,uuid,obsGroup:(uuid,id,voided),groupMembers:(id,uuid,voided),voided)';
